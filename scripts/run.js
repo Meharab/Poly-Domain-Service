96c019856a357bb5105457e2a53647928301e289
meharab <meharablatif@hotmail.com>
2022-11-23 19:26:40 +0600

    setting up smart contract

---
+++ scripts/run.js
@@ -0,0 +1,19 @@
+const main = async () => {
+    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+    const domainContract = await domainContractFactory.deploy();
+    await domainContract.deployed();
+    console.log("Contract deployed to:", domainContract.address);
+    console.log("Contract Object:", domainContract);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
