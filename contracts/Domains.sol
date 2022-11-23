01a91e8ba4082fc1f33a27a2881fa7c03ff983dc
meharab <meharablatif@hotmail.com>
2022-11-23 18:57:37 +0600

    setting up hardhat

---
+++ contracts/Domains.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Domains {
+  constructor() {
+    console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
+  }
+}
