ae0055fdda9e4f29d6c3d85f6b5fca1ca95c1cdd
meharab <meharablatif@hotmail.com>
2022-11-23 23:20:02 +0600

    setting up scripts

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,11 +1,46 @@
 // SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.10;
 
+// Don't forget to add this import
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
-  constructor() {
-    console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
+  // Here's our domain TLD!
+  string public tld;
+
+  mapping(string => address) public domains;
+  mapping(string => string) public records;
+		
+  // We make the contract "payable" by adding this to the constructor
+  constructor(string memory _tld) payable {
+    tld = _tld;
+    console.log("%s name service deployed", _tld);
   }
+		
+  // This function will give us the price of a domain based on length
+  function price(string calldata name) public pure returns(uint) {
+    uint len = StringUtils.strlen(name);
+    require(len > 0);
+    if (len == 3) {
+      return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+    } else if (len == 4) {
+      return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+    } else {
+      return 1 * 10**17;
+    }
+  }
+  // Added "payable" modifier to register function
+  function register(string calldata name) public payable{
+    require(domains[name] == address(0));
+    
+    uint _price = price(name);
+
+    // Check if enough Matic was paid in the transaction
+    require(msg.value >= _price, "Not enough Matic paid");
+
+    domains[name] = msg.sender;
+    console.log("%s has registered a domain!", msg.sender);
+  }
+  // Other functions unchanged
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,31 @@
 const main = async () => {
-    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
-    await domainContract.deployed();
-    console.log("Contract deployed to:", domainContract.address);
-    console.log("Contract Object:", domainContract);
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+  // The first return is the deployer, the second is a random account
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+  const domainContract = await domainContractFactory.deploy();
+  await domainContract.deployed();
+  console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let txn = await domainContract.register("doom");
+  await txn.wait();
+
+  const domainAddress = await domainContract.getAddress("doom");
+  console.log("Owner of domain doom:", domainAddress);
+
+  // Trying to set a record that doesn't belong to me!
+  txn = await domainContract.connect(randomPerson).setRecord("doom", "Haha my domain now!");
+  await txn.wait();
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
