f6e7946eda20d6035d886ec118f6bda5fef527c1
meharab <meharablatif@hotmail.com>
2022-11-26 20:23:38 +0600

    added env

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -27,7 +27,7 @@ contract Domains is ERC721URIStorage {
   mapping(string => address) public domains;
   mapping(string => string) public records;
 
-  constructor(string memory _tld) payable ERC721("Ninja Name Service", "NNS") {
+  constructor(string memory _tld) payable ERC721("Dope Name Service", "DNS") {
     tld = _tld;
     console.log("%s name service deployed", _tld);
   }
@@ -39,11 +39,11 @@ contract Domains is ERC721URIStorage {
     uint len = StringUtils.strlen(name);
     require(len > 0);
     if (len == 3) {
-      return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+      return 5 * 10**15; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
     } else if (len == 4) {
-      return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+      return 3 * 10**15; // To charge smaller amounts, reduce the decimals. This is 0.3
     } else {
-      return 1 * 10**17;
+      return 1 * 10**15;
     }
   }
   
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    }
+  }
 };
